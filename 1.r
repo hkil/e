b385d73c9ce0bbdeff0386ced596353ff898c93d
hkil <63327864+hkil@users.noreply.github.com>
2022-08-06 14:41:17 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -1,12 +1,12 @@
 
-wcf <- read.csv("https://raw.githubusercontent.com/hkil/m/master/wcf.csv")
+source("https://raw.githubusercontent.com/hkil/m/master/r3m.r")
 
 meta_tree(wcf, study, cont_type,
                 group, err_type, time,
-         abb_names = TRUE, 
+          abb_names = TRUE, 
          abb_length = 5,
-         num_names = TRUE,
+          num_names = TRUE,
          num_except = "study",
-         cex = 1.6,
-         cex_main = 1.7,
-         cex_top = 1.2)
+                cex = 1.6,
+           cex_main = 1.7,
+            cex_top = 1.2)
