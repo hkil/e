66d4c1b7af856793605369365ac18511a055fee4
hkil <63327864+hkil@users.noreply.github.com>
2022-08-06 14:45:32 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -1,14 +1,13 @@
 
 source("https://raw.githubusercontent.com/hkil/m/master/r3m.r")
 
-meta_tree(
-                wcf, study, cont_type,
-                group, err_type, time,
+meta_tree(data = wcf, study, cont_type,
+                 group, err_type, time,
           abb_names = TRUE, 
          abb_length = 5,
           num_names = TRUE,
          num_except = "study",
                 cex = 1.6,
            cex_main = 1.7,
-            cex_top = 1.2
-)
+            cex_top = 1.2)
+
