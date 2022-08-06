c12b9496923c7b13a7895396396892e48bec3167
hkil <63327864+hkil@users.noreply.github.com>
2022-08-06 14:27:39 -0500

    Create 1.r

---
+++ 1.r
@@ -0,0 +1,12 @@
+
+
+
+meta_tree(wcf, study, cont_type,
+                group, err_type, time,
+         abb_names = TRUE, 
+         abb_length = 5,
+         num_names = TRUE,
+         num_except = "study",
+         cex = 1.6,
+         cex_main = 1.7,
+         cex_top = 1.2)
