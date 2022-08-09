2889cf74aac3501a857f6ff1a7fdede8fad4a9f6
hkil <63327864+hkil@users.noreply.github.com>
2022-08-08 20:04:45 -0500

    Update 1.r

--- 1.r
+++ 1.r
@@ -3,13 +3,13 @@ source("https://raw.githubusercontent.com/hkil/m/master/r3m.r")
 
 # Note 'wcf' is a built-in dataset.
 
-meta_tree(data = wcf, study, cont_type,
+meta_tree(data = wcf, study, cont_type, # place 'study' first, and the design elements after that
                  group, err_type, time,
-          abb_names = TRUE, 
-         abb_length = 5,
-          num_names = TRUE,
-         num_except = "study",
-                cex = 1.6,
-           cex_main = 1.7,
-            cex_top = 1.2)
+          abb_names = TRUE,    # Abbreviate long-character codings
+         abb_length = 5,       # Allow up to 5 charachters to display after abbreviation
+          num_names = TRUE,    # Convert character codings to numerics for easier display
+         num_except = "study", # Don't convert variable 'study' to numerics
+                cex = 1.6,     # size of font in the visual
+           cex_main = 1.7,     # size of font in the visual's caption
+            cex_top = 1.2)     # size of font in the visual's top labels
 
